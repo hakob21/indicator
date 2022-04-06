6c279fd01c8f008e2ce0b18cf5e7f8bd77453a2c
hakob <hakob21@gmail.com>
2022-04-06 21:13:38 +0200

    initial commit

---
+++ src/main/java/com/bot/movingaveragebot/MovingAverageBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.movingaveragebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingAverageBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovingAverageBotApplication.class, args);
+    }
+
+}
